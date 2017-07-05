77dc3f4a38ddb0d47564af9afba171e224d2a67a
Zach Zundel <me@zachzundel.com>
2017-07-05 10:47:24 +0100

    Fix local settings import

--- src/digitalprofessors/settings.py
+++ src/digitalprofessors/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
-    pass
+    print("Not importing local settings")
