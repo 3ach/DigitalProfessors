cc9f567503f482290fa9187f620b91b1a6d2a780
Zach Zundel <me@zachzundel.com>
2017-08-19 04:13:36 -0600

    Register session category

--- src/tutoring/admin.py
+++ src/tutoring/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from tutoring.models import Client, Session, Tutor
+from tutoring.models import Client, Session, Tutor, SessionCategory
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Session)
+admin.site.register(SessionCategory)
 admin.site.register(Tutor)
